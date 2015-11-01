91b6f6b2b5cad8ec5e4d8b3bb9955b7d2ad179bb
sharath <sharuu123@gmail.com>
2015-11-01 10:12:49 -0500

    BM for only visible nodes

--- boltzmann.py
+++ boltzmann.py
@@ -8,11 +8,16 @@ class CreateBM:
 		self.w = 0.5*(self.w + self.w.transpose()) 		# Make w symmetric
 		for i in range(0,self.w.shape[0]):
 			self.w[i,i]=0
+		# self.w = np.insert(self.w, 0, 0, axis = 0)
+		# self.w = np.insert(self.w, 0, 0, axis = 1)
 		print(self.w)
 		# print(self.b)
 
 	def getWeights(self):
 		return self.w
+
+	def getBiasWeights(self):
+		return self.b
 
 	def GenerateSamples(self, num_samples, max_epochs=1000):
 		# Set all units to random binary states
@@ -29,6 +34,7 @@ class CreateBM:
 				nodes[node] = node_state
 			# Once thermal equilibrium is reached, put back the configuration
 			samples[i-1,:] = nodes
+			print('.', end="")
 		return samples
 
 	def _logistic(self, x):
@@ -46,11 +52,14 @@ class BM:
 		for i in range(0,self.w.shape[0]):
 			self.w[i,i]=0
 		self.b = 0.1 * np.random.randn(self.num_total, 1)
+		self.bias = np.ones((self.num_total, self.num_total))
 		# print(self.w)
 		# print(self.b)
 
-	def train(self, data, orig_w, max_epochs = 100):
+	def train(self, data, orig_w, orig_bw, max_epochs = 100):
 		data_size = data.shape[0]
+		error = np.sum((orig_w - self.w) ** 2)
+		print("error is %s" % (error))
 
 		for epoch in range(max_epochs):
 
@@ -58,38 +67,108 @@ class BM:
 
 			# For each training pattern
 			s = np.zeros((self.num_total, self.num_total))
+			db = np.zeros((self.num_total, 1))
 			nodes = np.zeros((1,self.num_total))
 			for i in range(1,data_size+1):
 				nodes = data[i-1,:]
 				nodes = nodes[None,:]
 				s += np.dot(nodes.T, nodes)
+				db += np.dot(self.bias, nodes.T)
 			pplus = s/data_size
-			print(pplus)
+			bplus = db/data_size
+			# print(pplus)
 
 			# Negative Phase
 
 			# Randomly set all the units to binary states
+			daydream = np.zeros((data_size, 6))
 			s = np.zeros((self.num_total, self.num_total))
+			db = np.zeros((self.num_total, 1))
 			for i in range(1,data_size+1):
 				nodes = np.random.random_integers(0,1,6)
-				for inner_epoch in range(1000):
+				for inner_epoch in range(10):
 					node = np.random.random_integers(0,5,1)
 					node_activation = self.b[node,0] + np.dot(nodes, self.w[:,node])
 					node_probability = self._logistic(node_activation)
 					node_state = node_probability > np.random.rand()
 					nodes[node] = node_state
+				nodes = nodes[None,:]
+				daydream[i-1,:] = nodes
 				s += np.dot(nodes.T, nodes)
-				# print(nodes)
+				db += np.dot(self.bias, nodes.T)
 			pminus = s/data_size
-			print(pminus)
+			bminus = db/data_size
+			# print(pminus)
 			
 			# Update Weights
 
 			self.w += self.learning_rate * (pplus - pminus)
+			self.b += self.learning_rate * (bplus - bminus)
 			for i in range(0,self.w.shape[0]):
 				self.w[i,i]=0
-			error = np.sum((orig_w - self.w) ** 2)
+			error1 = np.sum((orig_w - self.w) ** 2)
+			error2 = np.sum((orig_bw - self.b) ** 2)
+			error = error1 + error2
 			print("Epoch %s: error is %s" % (epoch, error))
+			derror = np.sum((data - daydream) ** 2)
+			print("Epoch %s: derror is %s" % (epoch, derror))
+
+	def trainWithHidden(self, data, orig_w, orig_bw, max_epochs = 100):
+		data_size = data.shape[0]
+		error = np.sum((orig_w - self.w) ** 2)
+		print("error is %s" % (error))
+
+		for epoch in range(max_epochs):
+
+			# Positive Phase
+
+			# For each training pattern
+			s = np.zeros((self.num_total, self.num_total))
+			db = np.zeros((self.num_total, 1))
+			nodes = np.zeros((1,self.num_total))
+			for i in range(1,data_size+1):
+				nodes = data[i-1,:]
+				nodes = nodes[None,:]
+				s += np.dot(nodes.T, nodes)
+				db += np.dot(self.bias, nodes.T)
+			pplus = s/data_size
+			bplus = db/data_size
+			# print(pplus)
+
+			# Negative Phase
+
+			# Randomly set all the units to binary states
+			daydream = np.zeros((data_size, 6))
+			s = np.zeros((self.num_total, self.num_total))
+			db = np.zeros((self.num_total, 1))
+			for i in range(1,data_size+1):
+				nodes = np.random.random_integers(0,1,6)
+				for inner_epoch in range(10):
+					node = np.random.random_integers(0,5,1)
+					node_activation = self.b[node,0] + np.dot(nodes, self.w[:,node])
+					node_probability = self._logistic(node_activation)
+					node_state = node_probability > np.random.rand()
+					nodes[node] = node_state
+				nodes = nodes[None,:]
+				daydream[i-1,:] = nodes
+				s += np.dot(nodes.T, nodes)
+				db += np.dot(self.bias, nodes.T)
+			pminus = s/data_size
+			bminus = db/data_size
+			# print(pminus)
+			
+			# Update Weights
+
+			self.w += self.learning_rate * (pplus - pminus)
+			self.b += self.learning_rate * (bplus - bminus)
+			for i in range(0,self.w.shape[0]):
+				self.w[i,i]=0
+			error1 = np.sum((orig_w - self.w) ** 2)
+			error2 = np.sum((orig_bw - self.b) ** 2)
+			error = error1 + error2
+			print("Epoch %s: error is %s" % (epoch, error))
+			derror = np.sum((data - daydream) ** 2)
+			print("Epoch %s: derror is %s" % (epoch, derror))
 
 	def _logistic(self, x):
 		return 1.0 / (1 + np.exp(-x))
@@ -97,10 +176,12 @@ class BM:
 if __name__ == '__main__':
 	c = CreateBM() # Generate Data
 	orig_w = c.getWeights()
-	samples = c.GenerateSamples(num_samples = 20, max_epochs = 5000)
-	print(samples)
-	r = BM(num_visible = 6, num_hidden = 0)
-	r.train(samples, orig_w, max_epochs = 10)
+	orig_bw = c.getBiasWeights()
+	samples = c.GenerateSamples(num_samples = 1000, max_epochs = 1000)
+	# print(samples)
+	r = BM(num_visible = 6, num_hidden = 0, learning_rate = 1)
+	r.train(samples, orig_w, orig_bw, max_epochs = 50)
 	print(r.w)
+	print(orig_w)
 	# user = np.array([[0,0,0,1,1,0]])
 	# print(r.run_visible(user))																																									
